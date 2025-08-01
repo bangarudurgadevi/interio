2e6b9911aa030e78754a3a3efaea5ec09fc1a3b9
bangarudurgadevi <durgadevibangaru165@gmail.com>
2025-08-01 15:06:24 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CategoriesComponent } from './navigatingcomponents/categories/categories.component';
+import { FurnitureComponent } from './navigatingcomponents/furniture/furniture.component';
+import { InteriordesignComponent } from './navigatingcomponents/interiordesign/interiordesign.component';
+import { PropertyComponent } from './navigatingcomponents/property/property.component';
+import { HomeComponent } from './navigatingcomponents/home/home.component';
+import { BannerComponent } from './navigatingcomponents/home/banner/banner.component';
+import { CategoryComponent } from './navigatingcomponents/home/category/category.component';
+import { WeekendComponent } from './navigatingcomponents/home/weekend/weekend.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CategoriesComponent,
+    FurnitureComponent,
+    InteriordesignComponent,
+    PropertyComponent,
+    HomeComponent,
+    BannerComponent,
+    CategoryComponent,
+    WeekendComponent
   ],
   imports: [
     BrowserModule,
